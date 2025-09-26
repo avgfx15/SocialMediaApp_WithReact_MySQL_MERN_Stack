4c7c8c131a3ccd233454314a66de46dfbfde54ef
avgfx15 <avgfx15@gmail.com>
2025-09-26 16:06:11 +0530

    Setup Tailwind and Daisuyi styling and complete Login Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
+// | Import Styling
 import './App.css';
+// | Import Components
+import LoginPage from './pages/loginPages/LoginPage';
 
+// & App Main Component
 const App = () => {
-  return <div>App</div>;
+  // ^ App Component Render
+  return (
+    <div className='w-full h-screen bg-sky-300'>
+      <LoginPage />
+    </div>
+  );
 };
 
+// ~ App Component Export
 export default App;
--- frontend/src/pages/loginPages/LoginPage.jsx
+++ frontend/src/pages/loginPages/LoginPage.jsx
@@ -4,7 +4,64 @@ import './login.scss';
 // & Login Page Component
 const LoginPage = () => {
   // ^ Login Page Render
-  return <div>LoginPage</div>;
+  return (
+    <div className='h-screen w-[75%] mx-auto flex justify-center items-center'>
+      <div className='card lg:card-side bg-base-100 shadow-sm h-[65%]'>
+        <figure className='w-1/2 border border-blue-950 overflow-hidden'>
+          <img
+            src='./Images/loginPage.png'
+            alt='Album'
+            className='w-full object-cover h-full rounded-4xl'
+          />
+        </figure>
+        <div className='p-5 w-1/2 flex items-center justify-center border border-blue-950 rounded-4xl'>
+          <div className='flex flex-col items-center justify-center w-full'>
+            <div className='mb-3'>
+              <h2 className='text-3xl text-blue-500'>Login</h2>
+            </div>
+            <form className='w-full'>
+              <div className='mb-3 flex flex-col justify-center w-full'>
+                <label className='text-xl text-blue-500 mb-3' htmlFor='email'>
+                  Email
+                </label>
+                <input
+                  type='email'
+                  id='email'
+                  placeholder='Enter Your Email'
+                  name='email'
+                  className='p-1 px-2 rounded-lg'
+                />
+              </div>
+              <div className='mb-3 flex flex-col justify-center w-full'>
+                <label
+                  className='text-xl text-blue-500 mb-3'
+                  htmlFor='password'
+                >
+                  Password
+                </label>
+                <input
+                  type='password'
+                  id='password'
+                  placeholder='Enter Your Password'
+                  name='password'
+                  className='p-1 px-2 rounded-lg'
+                />
+              </div>
+              <div className='mb-3 flex flex-col justify-center w-full'>
+                <button className='btn bg-blue-500'>Login</button>
+              </div>
+            </form>
+            <div className='mb-3'>
+              <p className='text-lg text-blue-500 mb-3 inline-block'>
+                Don't have an account? Please
+              </p>
+              <span className='hover:underline text-white ml-2'>Register</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 // ~ Login Page Export
